792907592890a27bfeba9f5d17b85823eff6a855
otgonbileg11 <otgonbileg1030@gmail.com>
2023-04-20 19:03:46 +0800

    about me style

---
+++ public/me.js
@@ -0,0 +1,24 @@
+class Otgonbileg{
+    // I'm ambitious and I love new challenges :(
+    // My vast variety of skills is continuously expanding.
+    constructor() {
+    this.name = 'Chimedtseren Otgonbileg'
+    this.dayOfBirthTimestamp = 19991030
+    this.email = 'otgonbileg1030@gmail.com'
+    }
+    workExperience() {
+    return ([
+    {'2023-2023': 'NIT - Developer class'},
+    {'2023-now': 'Front-End Developer at Nasha Tech LLC'}
+    ])
+    }
+    education() {
+    return ([
+    {'2016-2021': 'Mongolian University of Science and Technology - Mongolia - Civil'},
+    {'2019-2021': 'Nagaoka University of Technology - Japan - IT'}
+    ])
+    }
+    skills() {
+    return (['HTML/CSS/JS', 'Vue', 'Node.js', 'Bootstrap/Tailwind', 'Webpack/Vite', 'SCSS/Less', 'npm/yarn/pnpm', 'SSR', 'GIT/CVS', 'Web-extensions', 'Web Sockets', 'Firebase', 'PHP', 'MySQL/MongoDB/ORM', 'Wordpress', 'Photoshop', 'Illustrator', 'After Effects', 'Premiere', 'Motion design', 'UX/UI', 'C#', 'Unity', 'TypeScript', 'NestJS', 'Cypress', 'Jest', 'Nuxt' ])
+    }
+    }
--- src/components/about.js
+++ src/components/about.js
@@ -9,7 +9,7 @@ export default function About() {
             <div className="code-wrp">
                 <div data-line-nr="01" className="code-l">
                     <div className="methods">class</div>
-                    <span className="propname"> Otgonbileg</span>
+                    <span className="propname"> Anonymous</span>
                      &#123;
                 </div> 
                 <div data-line-nr="02" className="code-l">
@@ -30,20 +30,20 @@ export default function About() {
                     <div className="dot">····</div>
                     <span className="scope">this</span>.<span className="prop2">name</span>
                     <span className="scope"> = </span> 
-                    <span className="string">'Chimedtseren Otgonbileg'</span>
+                    <span className="string">'Micheal Jackson'</span>
                 </div> 
                 <div data-line-nr="06" className="code-l">
                     <div className="dot">····</div>
                     <span className="scope">this</span>.<span className="prop2">dayOfBirthTimestamp</span> 
                     <span className="scope"> = </span>
-                    <span className="number">602745592</span>
+                    <span className="number">10301963</span>
                 </div> 
                 <div data-line-nr="07" className="code-l">
                     <div className="dot">····</div>
                     <span className="scope">this</span>.
                     <span className="prop2">email</span> 
                     <span className="scope"> = </span>
-                    <span className="string">'otgonbileg1030@gmail.com'</span>
+                    <span className="string">'anonymous1963@gmail.com'</span>
                 </div>
                 <div data-line-nr="08" className="code-l">
                     <div className="dot">··</div>
@@ -62,49 +62,17 @@ export default function About() {
                 <div data-line-nr="11" className="code-l">
                     <div className="dot">······</div>
                     &#123;
-                    <span className="string">'2020-now'</span> 
+                    <span className="string">'2023-2023'</span> 
                     :
-                    <span className="string"> 'Full-stack Developer/Owner at Pixel Lab'</span>
-                    &#125;
-                </div> 
-                <div data-line-nr="12" className="code-l">
-                    <div className="dot">······</div>
-                    &#123;
-                    <span className="string">'2020-now'</span>
-                    :
-                    <span className="string"> 'Full-stack Developer at Digitree Group S.A.'</span>
-                    &#125;
-                </div> 
-                <div data-line-nr="13" className="code-l">
-                    <div className="dot">······</div>
-                    &#123;
-                    <span className="string">'2017-2020'</span>
-                    :
-                    <span className="string"> 'Front-end Developer at Digitree Group S.A.'</span>
+                    <span className="string"> 'NIT - Developer class'</span>
                     &#125;
                 </div> 
                 <div data-line-nr="14" className="code-l">
                     <div className="dot">······</div>
                     &#123;
-                    <span className="string">'2015-2017'</span>
+                    <span className="string">'2023-now'</span>
                     :
-                    <span className="string"> 'Full-Stack Designer at Digitree Group S.A.'</span>
-                    &#125;
-                </div>
-                <div data-line-nr="15" className="code-l">
-                    <div className="dot">······</div>
-                    &#123;
-                    <span className="string">'2013-2015'</span>
-                    :
-                    <span className="string"> 'Graphic Designer at Digitree Group S.A.'</span>
-                    &#125;
-                </div> 
-                <div data-line-nr="16" className="code-l">
-                    <div className="dot">······</div>
-                    &#123;
-                    <span className="string">'2006-now'</span>
-                    :
-                    <span className="string"> 'Full-Stack Designer/developer at Yasio.dev'</span>
+                    <span className="string"> 'Front-End Developer at Nasha Tech LLC'</span>
                     &#125;
                 </div>
                 <div data-line-nr="17" className="code-l">
@@ -129,14 +97,15 @@ export default function About() {
                     &#123;
                     <span className="string">'2012-2014'</span>
                     :
-                    <span className="string"> 'Wyższa Szkoła Biznesu w Dąbrowie Górniczej - mgr IT'</span>
+                    <span className="string"> 'Mongolian University of Science and Technology - Mongolia - IT'</span>
                     &#125;&#44;
                 </div> 
                 <div data-line-nr="22" className="code-l">
                     <div className="dot">······</div>
                     &#123;
-                    <span className="string">'2008-2012'</span>
-                    <span className="string"> 'Wyższa Szkoła Technologii Informatycznych w Katowicach - inż. IT'</span>
+                    <span className="string">'2019-2021'</span>
+                    :
+                    <span className="string"> 'Nagaoka University of Technology - Japan - IT'</span>
                     &#125;
                 </div>
                 <div data-line-nr="23" className="code-l">
@@ -156,7 +125,7 @@ export default function About() {
                     <div className="dot">····</div>
                     <div className="methods">return </div>
                     &#40;&#91; 
-                    <span className="string">'HTML/CSS/JS', 'Vue', 'Node.js', 'Bootstrap/Tailwind', 'Webpack/Vite', 'SCSS/Less', 'npm/yarn/pnpm', 'SSR', 'GIT/CVS', 'Web-extensions', 'Web Sockets', 'Firebase, 'PHP', 'MySQL/MongoDB/ORM', 'Wordpress', 'Photoshop', 'Illustrator', 'After Effects', 'Premiere', 'Motion design', 'UX/UI', 'C#', 'Unity', 'TypeScript', 'NestJS', 'Cypress', 'Jest', 'Nuxt' </span> 
+                    <span className="string">'HTML/CSS/JS', 'Vue', 'Node.js', 'Bootstrap/Tailwind', 'Webpack/Vite', 'SCSS/Less', 'npm/yarn/pnpm', 'SSR', 'GIT/CVS', 'Web-extensions', 'Web Sockets', 'Firebase', 'PHP', 'MySQL/MongoDB/ORM', 'Wordpress', 'Photoshop', 'Illustrator', 'After Effects', 'Premiere', 'Motion design', 'UX/UI', 'C#', 'Unity', 'TypeScript', 'NestJS', 'Cypress', 'Jest', 'Nuxt' </span> 
                     <span className="cursor"></span> 
                      &#93;&#41;
                 </div> 
